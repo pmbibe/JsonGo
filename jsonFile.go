99c7b466fde5f940f2ebb6eba1e4b2698b264200
unknown <ptminhduc18101997@gmail.com>
2020-10-13 16:18:37 +0700

    first commit

---
+++ jsonFile.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+type Option struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
+type Chapter struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []Option `json:"options"`
+}
+type StoryF struct {
+	Intros    Chapter `json:"intro"`
+	NewYork   Chapter `json:"intro"`
+	Debate    Chapter `json:"debate"`
+	SeanKelly Chapter `json:"sean-kelly"`
+	MarkBates Chapter `json:"mark-bates"`
+	Denver    Chapter `json:"denver"`
+	Home      Chapter `json:"home"`
+}
+
+func readFile(file string) string {
+	dat, _ := ioutil.ReadFile(file)
+	return string(dat)
+
+}
+func main() {
+	var storyF StoryF
+	data := readFile("A.json")
+	err := json.Unmarshal([]byte(data), &storyF)
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, err := json.Marshal(storyF)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var out bytes.Buffer
+	json.Indent(&out, b, "", "\t")
+	out.WriteTo(os.Stdout)
+
+}
